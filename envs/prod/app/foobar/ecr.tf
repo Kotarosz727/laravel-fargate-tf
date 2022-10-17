adf14361571c2183e445e3a0a0b6441b5111f918
kotarosz727 <kotarosz727@gmail.com>
2022-10-18 08:38:35 +0900

    PHP用ecrの作成

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -4,3 +4,10 @@ module "nginx" {
   name = "example-prod-foobar-nginx"
   hold_count = 5
 }
+
+module "php" {
+  source = "../../../modules/ecr"
+
+  name = "example-prod-foobar-php"
+  hold_count = 5
+}
