d79474cd6ca1e25e687bba5511f0a5afe3d9c697
kotarosz727 <kotarosz727@gmail.com>
2022-10-18 08:05:47 +0900

    nginxのECRにlifecyclepolicyを追加した

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -5,3 +5,26 @@ resource "aws_ecr_repository" "nginx" {
     Name = "nginx-created-20221017"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  repository = aws_ecr_repository.nginx.name
+
+    policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
